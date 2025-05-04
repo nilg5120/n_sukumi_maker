4ab0d92bff00ed10ddbb04232f4babc49144c533
ohkubo <ohkubo5120@gmail.com>
2025-05-04 21:05:08 +0900

    Update import statements to reflect project name change to n_sukumi_maker

--- test/people_input_page_test.dart
+++ test/people_input_page_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/main.dart';
+import 'package:n_sukumi_maker/main.dart';
 
 
 void main() {
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,9 @@ import 'package:n_sukumi_maker/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(
+      MaterialApp(home: PeopleInputPage()),
+    );
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
