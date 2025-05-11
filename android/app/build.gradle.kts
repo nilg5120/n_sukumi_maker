a2c8d1acab8fe4097c428bd11e7eab5de8e7cb48
ohkubo <ohkubo5120@gmail.com>
2025-05-11 18:29:10 +0900

    feat: keystoreと署名設定の追加
    - .gitignoreにkey.propertiesとkeystore関連ファイルの無視設定を追加
    - android/app/build.gradle.ktsにkeystoreの読み込みと署名設定を追加
    - android/gradlewとandroid/gradlew.batを新規作成

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,10 +5,36 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+import java.util.Properties
+import java.io.FileInputStream
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+
+println("🔐 key.properties 読み込み確認:")
+println("storePassword = ${keystoreProperties["storePassword"]}")
+println("keyPassword = ${keystoreProperties["keyPassword"]}")
+println("keyAlias = ${keystoreProperties["keyAlias"]}")
+println("storeFile = ${keystoreProperties["storeFile"]}")
+
 android {
     namespace = "com.example.flutter_application_1"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
+
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -31,10 +57,14 @@ android {
     }
 
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
